4c67339c227dfc76c9d2a078b60f3c9fd4b2a194
Rushil Venkateswar <rushilv14@gmail.com>
2022-08-12 09:57:24 +0530

    Restructure code

---
+++ A2/ass2_20CS30045.c
@@ -0,0 +1,22 @@
+// this program contains definitions for printInt, readInt, printFlt, readFlt, printStr
+#include "myl.h"
+
+int printStr(char *string){
+
+}
+
+int printInt(int n){
+
+}
+
+int readInt(int *n){
+
+}
+
+int readFlt(float *n){
+
+}
+
+int printFlt(float n){
+    
+}
