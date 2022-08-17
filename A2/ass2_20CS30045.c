d85247aa1dd4e60637b09a43538619013b2d76fe
Rushil V <rushilv14@gmail.com>
2022-08-17 11:32:45 +0530

    Complete printStr

--- A2/ass2_20CS30045.c
+++ A2/ass2_20CS30045.c
@@ -2,11 +2,21 @@
 #include "myl.h"
 
 int printStr(char *string){
+    // find length of string
+    int len=0;
+    while(string[len++] != '\0');
 
+    __asm__ __volatile__(
+        "movl $1, %%eax \n\t"
+        "movq $1, %%rdi \n\t"
+        "syscall \n\t"
+        :
+        :"S"(string), "d"(len)
+    ); // $4: write, $1: on stdin
 }
 
 int printInt(int n){
-
+    
 }
 
 int readInt(int *n){
