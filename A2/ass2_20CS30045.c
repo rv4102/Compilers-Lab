8f06af73b5dc001a162bc7de09e7f6e7df598486
Rushil V <rushilv14@gmail.com>
2022-08-18 16:49:46 +0530

    finish lib

--- A2/ass2_20CS30045.c
+++ A2/ass2_20CS30045.c
@@ -1,6 +1,7 @@
 // this program contains definitions for printInt, readInt, printFlt, readFlt, printStr
 #include "myl.h"
 #define BUFFER 20
+#define PRECISION 6
 
 int printStr(char *string){
     // find length of string
@@ -31,7 +32,7 @@ int readInt(int *n){
         :"S"(inputBuff), "d"(BUFFER)
     );
 
-    if(len < 0)
+    if(len <= 0)
         return ERR;
 
     int neg = 0, idx = 0;
@@ -39,9 +40,8 @@ int readInt(int *n){
         neg = 1; // set flag to be true
         idx++; // begin traversing from index 1
     }
-    else if(inputBuff[0] == '+'){
+    else if(inputBuff[0] == '+')
         idx++;
-    }
 
     long n1 = 0;
     while(idx < len && inputBuff[idx] != '\n'){
@@ -107,9 +107,124 @@ int printInt(int n){
 }
 
 int readFlt(float *n){
+    char inputBuff[BUFFER];
+    int len;
+    __asm__ __volatile__ (
+        "movl $0, %%eax \n\t" 
+        "movq $0, %%rdi \n\t"
+        "syscall \n\t"
+        : "=a"(len)
+        :"S"(inputBuff), "d"(BUFFER)
+    );
 
+    if(len <= 0)
+        return ERR;
+    
+    int neg = 0, idx = 0;
+    float n1 = 0;
+    if(inputBuff[0] == '-'){
+        neg = 1; // set flag to true
+        idx++; // begin traversing from index 1
+    }
+    else if(inputBuff[0] == '+')
+        idx++; // begin traversing from index 1
+
+    while(idx < len && inputBuff[idx] != '\n' && 
+            inputBuff[idx] != '.'){
+        if(inputBuff[idx] < '0' || inputBuff[idx] > '9')
+            return ERR;
+        int dig = (int)(inputBuff[idx] - '0');
+        n1 *= 10;
+        n1 += dig;
+        idx++;
+    }
+    // if there are digits after the decimal point, then join them to n
+    if(idx < len && inputBuff[idx] == '.'){
+        idx++;
+        float shift = 10.F;
+        while(idx < len && inputBuff[idx] != '\n'){
+            if(inputBuff[idx] < '0' || inputBuff[idx] > '9')
+                return ERR;
+            float dig = (float)(inputBuff[idx] - '0');
+            dig /= shift;
+            n1 += dig;
+            shift *= 10;
+            idx++;
+        }
+    }
+    n1 = (neg == 1)? -n1 : n1;
+    *n = n1;
+
+    return OK;
 }
 
 int printFlt(float n){
+    char buff[BUFFER];
+    int len = 0;
+    if(n < 0){
+        buff[len] = '-';
+        len++;
+        n = -n;
+    }
+
+    // find the integral part of the float number
+    int integral_part = (int)n;
+    n -= integral_part;
+
+    // load the integral part into character array
+    while(integral_part != 0){
+        buff[len] = (char)('0' + integral_part % 10);
+        integral_part /= 10;
+        len++;
+    }
+    if(len == 0 || buff[len-1] == '-')
+        buff[len++] = '0';
     
+    // reverse the the stored number
+    int leftPtr = 0, rightPtr = len-1;
+    if(buff[0] == '-'){
+        leftPtr = 1; // go to index=1 if negative number
+    }
+    while(leftPtr < rightPtr){
+        char temp = buff[leftPtr];
+        buff[rightPtr] = buff[rightPtr];
+        buff[leftPtr] = temp;
+        leftPtr++;
+        rightPtr--;
+    }
+
+    buff[len++] = '.';
+
+    // get the fractional part
+    for(int i=0; i<PRECISION; i++)
+        n *= 10;
+    int fractional_part = (int)n;
+
+    // store fractional part in character array
+    for(int i=0; i<PRECISION; i++){
+        buff[len++] = (char)('0' + fractional_part%10);
+        fractional_part /= 10;
+    }
+    leftPtr = len-1 - PRECISION;
+    rightPtr = len-1;
+    while(leftPtr < rightPtr){
+        char temp = buff[leftPtr];
+        buff[leftPtr] = buff[rightPtr];
+        buff[rightPtr] = temp;
+        leftPtr++;
+        rightPtr--;
+    }
+
+    while(buff[len-1] == '0')
+        len--;
+    
+    __asm__ __volatile__(
+        "movl $1, %%eax \n\t"
+        "movq $1, %%rdi \n\t"
+        "syscall \n\t"
+        :
+        : "S"(buff), "d"(len)
+    );
+    
+    return len;
 }
